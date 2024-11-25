c9c2aef21fde29429f9b2f164d82f5b60008af8f
meo-young <meoyoung000@naver.com>
2024-11-25 20:50:56 +0900

    DrawPanel 1 구현

---
+++ Assets/03. Scripts/Meoyoung/UI/DrawUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DrawUI : MonoBehaviour
+{
+    [SerializeField] DrawManager drawManager;
+    public void OnDrawBtnHandler()
+    {
+        if(!this.gameObject.activeSelf)
+            this.gameObject.SetActive(true);
+    }
+    public void OnPrevBtnHandler()
+    {
+        if(this.gameObject.activeSelf)
+            this.gameObject.SetActive(false);
+    }
+
+    public void OnContentBtnHandler(int _index)
+    {
+
+    }
+}
