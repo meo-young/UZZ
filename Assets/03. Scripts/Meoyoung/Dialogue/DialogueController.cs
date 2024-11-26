dc86cb35712041a6fac42ff9ea09679c44565efa
meo-young <meoyoung000@naver.com>
2024-11-27 00:07:29 +0900

    스토리 Transition 구현

--- Assets/03. Scripts/Meoyoung/Dialogue/DialogueController.cs
+++ Assets/03. Scripts/Meoyoung/Dialogue/DialogueController.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using static DialogueManager;
 
 public class DialogueController : MonoBehaviour
 {
     [SerializeField] int currentIndex;
+    [Header("# Fade Speed")]
+    [SerializeField] float fadeSpeed = 3f;
 
     [Header("# Dialogue UI Info")]
+    [SerializeField] GameObject blackImage;
+    [SerializeField] List<Sprite> backgroundImgs;
     [SerializeField] GameObject firstChoicePanel;
     [SerializeField] GameObject secondChoicePanel;
     [SerializeField] Text firstChoiceText;
     [SerializeField] Text secondChoiceText;
     [SerializeField] Text dialogueText;
     [SerializeField] Text nameText;
+    [SerializeField] Image backgroundImage;
 
     private DialogueInfo[] dialogues => DialogueManager.instance.dialogueInfos;
 
@@ -27,6 +33,7 @@ public class DialogueController : MonoBehaviour
     private void Start()
     {
         currentIndex = 0;
+        ChangeDialogueText();
     }
 
     public void OnFirstChoiceHandler()
@@ -55,6 +62,8 @@ public class DialogueController : MonoBehaviour
     {
         if (dialogues[currentIndex].type == Type.Choice)
             return;
+        CheckLastDialogue();
+        SetTransition();
 
         currentIndex++;
         ChangeDialogueText();
@@ -83,6 +92,8 @@ public class DialogueController : MonoBehaviour
 
     void ChangeDialogueText()
     {
+        ChangeBackgroundImage();
+
         if (dialogues[currentIndex].type == Type.Choice)
         {
             firstChoiceText.text = dialogues[currentIndex].dialogue1;
@@ -97,9 +108,66 @@ public class DialogueController : MonoBehaviour
         }
     }
 
+    void ChangeBackgroundImage()
+    {
+        backgroundImage.sprite = backgroundImgs[dialogues[currentIndex].backgroundIndex];
+    }
+
+    void SetTransition()
+    {
+        switch (dialogues[currentIndex].transition)
+        {
+            case 0:
+                StartCoroutine(FadeIn(fadeSpeed));
+                break;
+            case 1:
+                StartCoroutine(FadeOut(fadeSpeed));
+                break;
+        }
+    }
+
     void CheckLastDialogue()
     {
-        if (dialogues[currentIndex].nextIndex1 == 0)
-            return;
+        if (dialogues[currentIndex].nextIndex1 == -1)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    IEnumerator FadeOut(float _speed)
+    {
+        Color color = blackImage.GetComponent<Image>().color;
+        color.a = 0f;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < _speed)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(0f, 1f, elapsedTime / _speed);
+            blackImage.GetComponent<Image>().color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        blackImage.GetComponent<Image>().color = color;
+    }
+
+    IEnumerator FadeIn(float _speed)
+    {
+        Color color = blackImage.GetComponent<Image>().color;
+        color.a = 1f;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < _speed)
+        {
+            elapsedTime += Time.deltaTime;
+            color.a = Mathf.Lerp(1f, 0f, elapsedTime / _speed);
+            blackImage.GetComponent<Image>().color = color;
+            yield return null;
+        }
+
+        color.a = 0f;
+        blackImage.GetComponent<Image>().color = color;
     }
 }
