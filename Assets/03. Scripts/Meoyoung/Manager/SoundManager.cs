ab09aa8d1a1b66eb285cd2982c2601e24ff14fee
meo-young <meoyoung000@naver.com>
2024-12-01 19:01:03 +0900

    Main 꼬인 부분 수정

---
+++ Assets/03. Scripts/Meoyoung/Manager/SoundManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public enum SoundName
+    {
+        p_idle = 0,
+        p_sleep = 1,
+        p_walk = 2,
+        p_telep = 3,
+        p_down = 4,
+        p_sing = 5,
+        p_watch = 6,
+        p_window = 7,
+        p_starr = 8,
+        p_hi = 9,
+        p_mung = 10,
+        p_slime = 11,
+        p_dance = 12,
+        p_fly = 13,
+        p_bhi = 14,
+        p_groo = 15,
+        p_bubi = 16,
+        p_light = 17,
+        p_hand = 18,
+        p_screen = 19,
+        p_watering = 20,
+        p_spade = 21,
+        p_fertilizer = 22,
+        p_scissor = 23,
+        p_nutritional = 24,
+        p_giftr = 25,
+        p_present = 26,
+        p_bigev = 27,
+        p_miniev = 28
+    }
+}
